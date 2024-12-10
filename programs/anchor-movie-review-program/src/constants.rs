6d7091f5a80c8b549126b55bf76388104c007a98
chenqin <490358423@qq.com>
2024-12-10 12:01:49 +0800

    anchor demo

---
+++ programs/anchor-movie-review-program/src/constants.rs
@@ -0,0 +1,5 @@
+pub const MIN_RATING: u8 = 1;
+pub const MAX_RATING: u8 = 5;
+pub const MAX_TITLE_LENGTH: usize = 20;
+pub const MAX_DESCRIPTION_LENGTH: usize = 50;
+pub const DISCRIMINATOR: usize = 8;
---
+++ tests/anchor-movie-review-program.ts
@@ -0,0 +1,68 @@
+import * as anchor from "@coral-xyz/anchor";
+import { Program } from "@coral-xyz/anchor";
+import { expect } from "chai";
+import { AnchorMovieReviewProgram } from "../target/types/anchor_movie_review_program";
+ 
+describe("anchor-movie-review-program", () => {
+  // Configure the client to use the local cluster.
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
+ 
+  const program = anchor.workspace
+    .AnchorMovieReviewProgram as Program<AnchorMovieReviewProgram>;
+ 
+  const movie = {
+    title: "Just a test movie",
+    description: "Wow what a good movie it was real great",
+    rating: 5,
+  };
+ 
+  const [moviePda] = anchor.web3.PublicKey.findProgramAddressSync(
+    [Buffer.from(movie.title), provider.wallet.publicKey.toBuffer()],
+    program.programId,
+  );
+ 
+  it("Movie review is added`", async () => {
+    // Add your test here.
+    const tx = await program.methods
+      .addMovieReview(movie.title, movie.description, movie.rating)
+      .rpc();
+   
+    const account = await program.account.movieAccountState.fetch(moviePda);
+    expect(movie.title === account.title);
+    expect(movie.rating === account.rating);
+    expect(movie.description === account.description);
+    expect(account.reviewer === provider.wallet.publicKey);
+  });
+ 
+  it("Movie review is updated`", async () => {
+    const newDescription = "Wow this is new";
+    const newRating = 4;
+   
+    const tx = await program.methods
+      .updateMovieReview(movie.title, newDescription, newRating)
+      .rpc();
+   
+    const account = await program.account.movieAccountState.fetch(moviePda);
+    expect(movie.title === account.title);
+    expect(newRating === account.rating);
+    expect(newDescription === account.description);
+    expect(account.reviewer === provider.wallet.publicKey);
+  });
+ 
+  it("Deletes a movie review", async () => {
+    const tx = await program.methods.deleteMovieReview(movie.title).rpc();
+  });
+});
+
+/*
+
+  anchor-movie-review-program
+    ✔ Movie review is added` (209ms)
+    ✔ Movie review is updated` (468ms)
+    ✔ Deletes a movie review (470ms)
+
+
+  3 passing (1s)
+
+*/
